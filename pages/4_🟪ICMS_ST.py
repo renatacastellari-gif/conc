b61d80d5e48d1f996a90bb2d2ce35f9623b4630a
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-30 14:47:24 -0300

    Update 4_🟪ICMS_ST.py

--- pages/4_🟪ICMS_ST.py
+++ pages/4_🟪ICMS_ST.py
@@ -27,11 +27,13 @@ h3 {
 st.image('teste.svg', width=200) 
 
 
+
 st.markdown("""
 <div style='text-align: center;'>
     teste.svg
 </div>
 """, unsafe_allow_html=True)
+
 
 
 st.write("")
@@ -87,6 +89,7 @@ Nas operações de venda com CFOP <strong>6.403</strong>, o ICMS-ST é recolhido
 </div>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
