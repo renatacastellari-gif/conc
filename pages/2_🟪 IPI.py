31d55a56e6dc631716e2812b16ca2f80eeabfd6b
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-30 10:36:26 -0300

    Update 2_🟪 IPI.py

--- pages/2_🟪 IPI.py
+++ pages/2_🟪 IPI.py
@@ -48,7 +48,7 @@ st.markdown("""
     color:#FFFFFF;
     font-size:16px;">
 <ul style="list-style-type:none; padding-left:0;">
-<li> Competência <strong>06/2025</strong> (pagamento em 07/2025).</li>
+<li> Competência <strong>06/2025</strong> (pagamento em 07/2025):</li>
 <li>⚠️ Filial <strong>008</strong>: Identificado pagamento de <span style="color:#FFA500;">R$ 14.174,76 a maior</span>.</li>
 <li>⚠️ Filial <strong>019</strong>: Identificado pagamento de <span style="color:#FFA500;">R$ 6.463,23 a maior</span>. <br><strong>Status:</strong> Fiscal ciente, verificar compensação.</li>
 <li><strong>Sugestão:</strong> Automatizar o processo que envia esses valores para DCTFWeb para evitar digitação manual.</li>
@@ -58,4 +58,5 @@ st.markdown("""
 </ul>
 </div>
 """, unsafe_allow_html=True)
+
 
