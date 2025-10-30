2d7b3e9be0e7dd1db5349e44037d37e79727050e
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-30 11:35:51 -0300

    Update 1_🟪ICMS.py

--- pages/1_🟪ICMS.py
+++ pages/1_🟪ICMS.py
@@ -81,6 +81,8 @@ Diferenças de Pagamentos
 </h5>
 """, unsafe_allow_html=True)
 
+st.markdown("<div style='font-weight:bold; margin-bottom:8px;'>ICMS Próprio 06/2025:</div>", unsafe_allow_html=True)
+
 texto_diferencas = """
 
 
@@ -125,6 +127,7 @@ Entre 01/2025 e 08/2025, foram identificadas 559 notas com divergências entre r
 </ul>
 """
 st.markdown(f"<div class='card'>{color_numbers(texto_recuperar)}</div>", unsafe_allow_html=True)
+
 
 
 
