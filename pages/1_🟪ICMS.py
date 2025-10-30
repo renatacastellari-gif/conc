7dfdeadc53b9632815fd746d2d221c157dfc401f
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-30 11:33:25 -0300

    Update 1_🟪ICMS.py

--- pages/1_🟪ICMS.py
+++ pages/1_🟪ICMS.py
@@ -83,9 +83,10 @@ Diferenças de Pagamentos
 
 texto_diferencas = """
 
-<div style="margin-bottom:8px; font-weight:bold;">ICMS Próprio 06/2025:</div>
+
 
 <ul>
+<li> ICMS Próprio 06/2025:</li>
 <li>⚠️ Filial 019: pagou R$ 910,20 a maior, devido à retificação tardia.<br>
 DARE ICMS Próprio: Valor retificado de R$ 27.303,93 para R$ 26.393,73.<br>
 Verificar status com fiscal.</li>
@@ -124,6 +125,7 @@ Entre 01/2025 e 08/2025, foram identificadas 559 notas com divergências entre r
 </ul>
 """
 st.markdown(f"<div class='card'>{color_numbers(texto_recuperar)}</div>", unsafe_allow_html=True)
+
 
 
 
