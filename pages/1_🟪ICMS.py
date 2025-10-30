96b98ab9f883b7c38ae81af8c4bb23693665599c
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-30 14:50:29 -0300

    Update 1_🟪ICMS.py

--- pages/1_🟪ICMS.py
+++ pages/1_🟪ICMS.py
@@ -36,17 +36,6 @@ ul li {
 
 # Cabeçalho com logo
 
-# CSS para centralizar qualquer imagem
-st.markdown("""
-<style>
-div[data-testid="stImage"] {
-    display: flex;
-    justify-content: center;
-}
-</style>
-""", unsafe_allow_html=True)
-
-# Exibir imagem
 
 
 
@@ -148,6 +137,7 @@ Entre 01/2025 e 08/2025, foram identificadas 559 notas com divergências entre r
 </ul>
 """
 st.markdown(f"<div class='card'>{color_numbers(texto_recuperar)}</div>", unsafe_allow_html=True)
+
 
 
 
