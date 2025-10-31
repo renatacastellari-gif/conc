1c04bc92ee336300dbd907639a89ae632b762b31
renatacastellari-gif <renatacastellari@gmail.com>
2025-10-31 12:48:18 -0300

    Update bi.py

--- pages/bi.py
+++ pages/bi.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.title("Dashboard de Faturamento Mensal")
+st.title("Teste de Embed Power BI")
 
 components.html(
     """
     <iframe title="faturamento_mensal_" width="1140" height="541.25"
     src="https://app.powerbi.com/reportEmbed?reportId=41b18a5f-d294-4305-a56f-714a00efb81f&autoAuth=true&ctid=7c4d239e-9534-4e05-bb0b-ce4e8c32814a"
-    frameborder="0
+    frameborder="0" allowFullScreen="
